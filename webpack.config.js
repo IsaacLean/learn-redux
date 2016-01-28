68d2b46b6bac7ceccf546cfd6c58f44cc2a47a0f
Matthew Isaac Lean <matt@mattlean.com>
2016-01-28 01:09:19 -0800

    Finish basic setup for webpack with React

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,18 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const TARGET = process.env.npm_lifecycle_event;
-const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
-};
-
-const common = {
-    // Entry accepts a path or an object of entries.
-    entry: PATHS.app,
+module.exports = {
+    entry: './app/components/Main.js',
     output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
+        filename: 'public/bundle.js'
     },
     module: {
-        loaders: [{
-            test: /\.css$/,
-            loaders: ['style', 'css'],
-            include: PATHS.app
-        }]
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
     }
 };
-
-if(TARGET === 'start' || !TARGET) {
-    module.exports = merge(common, {
-        devServer: {
-            contentBase: PATHS.build,
-
-            // Enable sourcemaps
-            devtool: 'eval-source-map',
-
-            // Enable history API fallback so HTML5 History API based
-            // routing works. This is a good default that will come
-            // in handy in more complicated setups.
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            progress: true,
-
-            // Display only errors to reduce the amount of output.
-            stats: 'errors-only',
-
-            // Prase host and port from env so this is easy to customize.
-            host: process.env.HOST,
-            port: process.env.PORT
-        },
-
-        plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ]
-    });
-}
-
-if(TARGET === 'build') {
-    module.exports = merge(common, {});
-}
