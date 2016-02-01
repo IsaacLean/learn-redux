dcacdc8d9407bc14cee2f965e8c5d5e1292b34a9
Matthew Isaac Lean <matt@mattlean.com>
2016-02-01 12:49:04 -0800

    Restart fresh again

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Component } from 'react';
 
-import SearchBar from '../containers/search_bar';
-import WeatherList from '../containers/weather_list';
-
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <SearchBar />
-                <WeatherList />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,9 @@
-// module.exports = {
-//     entry: ['./src/index.js'],
-//     output: {
-//         path: __dirname,
-//         publicPath: '/',
-//         filename: 'bundle.js'
-//     },
-//     module: {
-//         loaders: [{
-//             exclude: /node_modules/,
-//             loader: 'babel'
-//         }]
-//     },
-//     resolve: {
-//         extensions: ['', '.js', '.jsx']
-//     },
-//     devServer: {
-//         historyApiFallback: true,
-//         contentBase: './'
-//     }
-// };
-
 module.exports = {
-    entry: ['./src/counter.js'],
+    entry: ['./src/index.js'],
     output: {
         path: __dirname,
         publicPath: '/',
-        filename: 'counterCompiled.js'
+        filename: 'bundle.js'
     },
     module: {
         loaders: [{
