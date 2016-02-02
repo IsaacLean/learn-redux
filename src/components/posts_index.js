d05fecf8fb58d257751431049ce88ffb8a981a0a
Matthew Isaac Lean <matt@mattlean.com>
2016-02-01 17:31:57 -0800

    Create new post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-    return <div>List of blog posts</div>;
-};
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div className="text-xs-right">
+                <Link to="/posts/new" className="btn btn-primary">
+                    Add a Post
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
